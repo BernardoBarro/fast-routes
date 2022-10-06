4bfe4be75aad26d06783cf43dea2b3f7cc95e940
Bernardo Witkoski <bernardowitkoski@hotmail.com>
2022-10-06 20:42:02 -0300

    Implementing register

--- lib/views/PageRegisterMotorista.dart
+++ lib/views/PageRegisterMotorista.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 import 'package:fast_routes/views/PageRegisterMotorista.dart';
+import 'package:intl/intl.dart';
 
 class PageRegisterMotorista extends StatefulWidget {
   const PageRegisterMotorista({Key? key}) : super(key: key);
@@ -25,9 +26,47 @@ class _PageRegisterMotoristaState extends State<PageRegisterMotorista> {
   var maskDate = MaskTextInputFormatter(mask: '##/##/####');
   bool _showPassword = false;
 
-  _registerMotorista() {
+  TextEditingController _controllerNome = TextEditingController();
+  TextEditingController _controllerCPF = TextEditingController();
+  TextEditingController _controllerTelefone = TextEditingController();
+  TextEditingController _controllerDataNascimento = TextEditingController();
+  TextEditingController _controllerEmail = TextEditingController();
+  TextEditingController _controllerSenha = TextEditingController();
+  
+  String _errorMessage = " ";
 
-    
+  _validateFieldsMotorista() {
+    String nome = _controllerNome.text;
+    String cpf = _controllerCPF.text;
+    String telefone = _controllerTelefone.text;
+    String dataNascimento = _controllerDataNascimento.text;
+    String email = _controllerEmail.text;
+    String senha = _controllerSenha.text;
+
+    if(nome.isNotEmpty) {
+      if(cpf.isNotEmpty && cpf.length == 11) {
+          if(telefone.isNotEmpty) {
+            if(dataNascimento.isNotEmpty) {
+              if(email.isNotEmpty) {
+                if(senha.isNotEmpty && senha.length >= 6) {
+                  _registerMotorista(nome, cpf, telefone, dataNascimento, email, senha);
+                } else {
+                  _errorMessage = "Erro";
+                }
+              }
+            }
+        }
+      }
+    }
+
+    _registerMotorista(String nome, String cpf, String telefone, String dataNascimento, String email, String senha) {
+      FirebaseDatabase db = FirebaseDatabase.instance;
+
+      var dateFormat = new DateFormat('yyyy-MM-dd');
+      //String dataFormatada = dateFormat.format(dataNascimento);
+      DateFormat dataFormatada = DateFormat(dataNascimento);
+  }
+
 
     Navigator.push(context,MaterialPageRoute(builder: ((context) => const LoginandRegister())));
   }
@@ -445,7 +484,7 @@ class _PageRegisterMotoristaState extends State<PageRegisterMotorista> {
                       onPrimary: Colors.white,
                       elevation: 0,
                     ),
-                    onPressed: _registerMotorista,
+                    onPressed: _validateFieldsMotorista,
                     child: Text(
                       "ENVIAR",
                       style: TextStyle(
