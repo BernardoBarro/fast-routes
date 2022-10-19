d30272cdafb0f55ef748abcb09e754d7e0c33e64
BernardoWitkoski <bernardowitkoski@hotmail.com>
2022-10-19 01:54:40 -0300

    Implementing reset Password onTap

--- lib/views/PageResetPassword.dart
+++ lib/views/PageResetPassword.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:email_validator/email_validator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class PageResetPassword extends StatefulWidget {
   const PageResetPassword({Key? key}) : super(key: key);
@@ -10,10 +12,27 @@ class PageResetPassword extends StatefulWidget {
 
 class _PageResetPasswordState extends State<PageResetPassword> {
   TextEditingController _controllerEmail = TextEditingController();
+  String _mensagemErro = "teste";
+  final formKey = GlobalKey<FormState>();
 
-  _resetPassword(String email) {
-    
+  _resetPassword(String emailField) {
+    FirebaseAuth auth = FirebaseAuth.instance;
+      auth
+        .sendPasswordResetEmail(email: emailField)
+        .catchError((error) {
+          if(error.code.toString() == "email-already-in-use") {
+                setState(() {
+                  _mensagemErro = "E-mail já cadastrado";
+                  print(error.toString());
+                });
+            } else {
+                setState(() {
+                  _mensagemErro = "ocorreu um erro: $error";
+                });
+              }                 
+        });      
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -26,39 +45,42 @@ class _PageResetPasswordState extends State<PageResetPassword> {
           child: Container(
             padding: EdgeInsets.only(top: 0, left: 32, right: 32),
             color: Color.fromRGBO(69, 69, 85, 1),
-            child: ListView(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(top: 60, left: 32, right: 32),
-                  child: SizedBox(
-                    width: 200,
-                    height: 180,
-                    child: Image.asset(
-                      "assets/images/logo.png",
+            child: Form(
+              child: ListView(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(top: 60, left: 32, right: 32),
+                    child: SizedBox(
+                      width: 200,
+                      height: 180,
+                      child: Image.asset(
+                        "assets/images/logo.png",
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 60, left: 32, right: 32),
-                  child: SizedBox(
-                    width: 200,
-                    height: 100,
-                    child: Text (
-                      "Insira o seu e-mail para mudar sua senha",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: Colors.white, fontSize: 20.0),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 60, left: 32, right: 32),
+                    child: SizedBox(
+                      width: 200,
+                      height: 100,
+                      child: Text(
+                        "Insira o seu e-mail para mudar sua senha",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.white, fontSize: 20.0),
                       ),
-                  ),                  
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  controller: _controllerEmail,
-                  validator: (email) {
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+
+                  //TEXT EMAIL
+                  TextFormField(
+                    controller: _controllerEmail,
+                    validator: (email) {
                       if (email == null || email.isEmpty) {
                         return 'Digite o seu E-mail';
                       } else if (!EmailValidator.validate(email)) {
@@ -66,68 +88,85 @@ class _PageResetPasswordState extends State<PageResetPassword> {
                       }
                       return null;
                     },
-                  // autofocus: true,
-                  keyboardType: TextInputType.emailAddress,
-                  style: TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
+                    // autofocus: true,
+                    keyboardType: TextInputType.emailAddress,
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
 
-                      //STYLE BORDER
-                      focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                        color: Color.fromRGBO(170, 170, 170, 1),
-                      )),
-                      enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                        color: Color.fromRGBO(170, 170, 170, 1),
-                      )),
+                        //STYLE BORDER
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                          color: Color.fromRGBO(170, 170, 170, 1),
+                        )),
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                          color: Color.fromRGBO(170, 170, 170, 1),
+                        )),
 
-                      //LABELS
-                      labelText: "E-mail",
-                      hintText: "Ex: email@email.com",
+                        //LABELS
+                        labelText: "E-mail",
+                        hintText: "Ex: email@email.com",
 
-                      //Style icon
-                      prefixIcon: Icon(
-                        Icons.person,
-                        color: Color.fromRGBO(170, 170, 170, 1),
-                      ),
-
-                      //Style Label
-                      labelStyle: TextStyle(
-                        color: Color.fromRGBO(170, 170, 170, 1),
-                        fontWeight: FontWeight.w400,
-                        fontSize: 18,
-                      ),
-
-                      //Style Hint
-                      hintStyle: TextStyle(
-                        color: Color.fromRGBO(255, 255, 255, 0.4),
-                        fontWeight: FontWeight.w400,
-                        fontSize: 18,
-                      )),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                //BUTTON
-                SizedBox(
-                    height: 50,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: Color.fromRGBO(51, 101, 229, 1),
-                        onPrimary: Colors.white,
-                        elevation: 0,
-                      ),
-                      onPressed: () {
-                        _resetPassword(_controllerEmail.text);
-                      },
-                      child: Text(
-                        "ENVIAR",
-                        style: TextStyle(
-                          fontFamily: 'InriaSans',
+                        //Style icon
+                        prefixIcon: Icon(
+                          Icons.person,
+                          color: Color.fromRGBO(170, 170, 170, 1),
                         ),
+
+                        //Style Label
+                        labelStyle: TextStyle(
+                          color: Color.fromRGBO(170, 170, 170, 1),
+                          fontWeight: FontWeight.w400,
+                          fontSize: 18,
+                        ),
+
+                        //Style Hint
+                        hintStyle: TextStyle(
+                          color: Color.fromRGBO(255, 255, 255, 0.4),
+                          fontWeight: FontWeight.w400,
+                          fontSize: 18,
+                        )),
+                  ),
+                  //MESSAGE ERROR
+                  Center(
+                    child: Text(
+                      _mensagemErro,
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 16,
                       ),
-                    )),                
-              ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  //BUTTON
+                  SizedBox(
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Color.fromRGBO(51, 101, 229, 1),
+                          onPrimary: Colors.white,
+                          elevation: 0,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _mensagemErro = "";
+                          });
+                          if (formKey.currentState!.validate()) {
+                            _resetPassword(
+                              _controllerEmail.text);
+                          }
+                        },
+                        child: Text(
+                          "ENVIAR",
+                          style: TextStyle(
+                            fontFamily: 'InriaSans',
+                          ),
+                        ),
+                      )),
+                ],
+              ),
             ),
           ),
         ));
