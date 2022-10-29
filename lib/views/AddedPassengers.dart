1f46b4a18b15a6cc6aa5a385136a99875889e5dd
Bernardo Barro <bernardo.barro@gmail.com>
2022-10-29 16:42:54 -0300

    feat search

--- lib/views/AddedPassengers.dart
+++ lib/views/AddedPassengers.dart
@@ -2,6 +2,10 @@
 
 import 'package:flutter/material.dart';
 
+import '../models/Customer.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 class AddedPassengers extends StatefulWidget {
   const AddedPassengers({Key? key}) : super(key: key);
 
@@ -12,6 +16,22 @@ class AddedPassengers extends StatefulWidget {
 TextEditingController editingController = TextEditingController();
 
 class _AddedPassengersState extends State<AddedPassengers> {
+  User? usuarioLogado = FirebaseAuth.instance.currentUser;
+  final db = FirebaseDatabase.instance.ref("passageiros");
+  static List<Customer> passageirosList = [];
+
+  List<Customer> displayList = List.from(passageirosList);
+
+  void updateList(String value) {
+    getPassagers();
+    setState(() {
+      displayList = passageirosList
+          .where((element) =>
+              element.nome.toLowerCase().contains(value.toLowerCase()))
+          .toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,56 +45,75 @@ class _AddedPassengersState extends State<AddedPassengers> {
             width: double.infinity,
             color: const Color.fromRGBO(69, 69, 85, 1),
             padding: const EdgeInsets.only(top: 20, right: 16, left: 16),
-            child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(7.0),
               child: Column(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.all(7.0),
-                    child: TextField(
-                      onChanged: (text) {},
-                      controller: editingController,
-                      style: const TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                        //Style Label
-                        labelStyle: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w400,
-                          fontSize: 15,
-                        ),
-
-                        //Style Hint
-                        hintStyle: const TextStyle(
-                          color: Color.fromRGBO(255, 255, 255, 0.4),
-                          fontWeight: FontWeight.w400,
-                          fontSize: 15,
-                        ),
-                        labelText: "Procurar passageiros",
-                        hintText: "Informe o nome do passageiro",
-                        prefixIcon: Icon(
-                          Icons.search,
-                          color: Colors.white,
-                        ),
-                        focusedBorder: const OutlineInputBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(25.0),
-                            ),
-                            borderSide: BorderSide(
-                              color: Color.fromRGBO(255, 255, 255, 1),
-                            )),
-                        enabledBorder: const OutlineInputBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(25.0),
-                            ),
-                            borderSide: BorderSide(
-                              color: Color.fromRGBO(255, 255, 255, 1),
-                            )),
+                  TextField(
+                    onChanged: (value) => updateList(value),
+                    controller: editingController,
+                    style: const TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      //Style Label
+                      labelStyle: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 15,
                       ),
+
+                      //Style Hint
+                      hintStyle: const TextStyle(
+                        color: Color.fromRGBO(255, 255, 255, 0.4),
+                        fontWeight: FontWeight.w400,
+                        fontSize: 15,
+                      ),
+                      labelText: "Procurar passageiros",
+                      hintText: "Informe o nome do passageiro",
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      ),
+                      focusedBorder: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(25.0),
+                          ),
+                          borderSide: BorderSide(
+                            color: Color.fromRGBO(255, 255, 255, 1),
+                          )),
+                      enabledBorder: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(25.0),
+                          ),
+                          borderSide: BorderSide(
+                            color: Color.fromRGBO(255, 255, 255, 1),
+                          )),
                     ),
                   ),
+                  SizedBox(height: 20.0),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: displayList.length,
+                      itemBuilder: (context, index) => ListTile(
+                        title: Text(displayList[index].nome),
+                        subtitle: Text(displayList[index].email),
+                      ),
+                    ),
+                  )
                 ],
               ),
             ),
           ),
         ));
+  }
+
+  void getPassagers() {
+    db.onValue.listen((event) {
+      final allTravels =
+          Map<String, dynamic>.from(event.snapshot.value as dynamic);
+      passageirosList = allTravels.values
+          .map((travelAsJSON) => Customer.fromRTDB(
+              Map<String, dynamic>.from(travelAsJSON), usuarioLogado!.uid))
+          .toList();
+    });
   }
 }
